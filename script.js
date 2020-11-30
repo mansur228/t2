b8395698715a0d01e9357afb2daf69d38f6cb00f
mansur228 <60790177+mansur228@users.noreply.github.com>
2020-11-30 19:35:12 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -27,29 +27,29 @@ $(document).ready(function() {
         })
     }
 
-    // deploy header_item
+    // deploy header_items
     
-    document.getElementsByClassName('header_menu_items')[0].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[0].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[0].classList.toggle('active')
     });
 
-    document.getElementsByClassName('header_menu_items')[1].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[1].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[1].classList.toggle('active')
     });
 
-    document.getElementsByClassName('header_menu_items')[2].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[2].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[2].classList.toggle('active')
     });
 
-    document.getElementsByClassName('header_menu_items')[3].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[3].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[3].classList.toggle('active')
     });
 
-    document.getElementsByClassName('header_menu_items')[4].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[4].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[4].classList.toggle('active')
     });
 
-    document.getElementsByClassName('header_menu_items')[5].addEventListener('click' , function() {
+    document.getElementsByClassName('header_menu_items')[5].addEventListener('click' , () => {
         $('.header_menu_items_deployed_vertion')[5].classList.toggle('active')
     });
 
